58d2078e9db64b4f8c21fa8ca847a233a2271abc
s-tangh <112664972+s-tangh@users.noreply.github.com>
2023-12-14 14:57:25 -0800

    Update Launcher.java

--- Launcher.java
+++ Launcher.java
@@ -1,6 +1,9 @@
 package Main;
+import java.io.IOException;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class Launcher {
 	public static void main(String[] args)
@@ -8,8 +11,19 @@ public class Launcher {
 		SwingUtilities.invokeLater(new Runnable()
 		{
 			public void run() {
+				try {
+				    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				        if ("Nimbus".equals(info.getName())) {
+				            UIManager.setLookAndFeel(info.getClassName());
+				            break;
+				        }
+				    }
+				} catch (Exception e) {
+				    // If Nimbus is not available, you can set the GUI to another look and feel.
+				}
+
 				MainWindow main = new MainWindow();
-				main.initialize();
+
 				
 			}
 		}
