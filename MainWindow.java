242d233b39425f067e833969383f6e63e9b1409e
s-tangh <112664972+s-tangh@users.noreply.github.com>
2024-01-11 14:58:32 -0800

    Update MainWindow.java

--- MainWindow.java
+++ MainWindow.java
@@ -9,40 +9,9 @@ public class MainWindow implements ActionListener{
 	private JFrame window ;
 	public MainWindow() 
 	{
-		initializeLogin();
+		initializeMain();
 	}
-	public void initializeLogin()
-	{		
-		window = new JFrame();
-		window.setTitle("Login");
-		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		window.setLocationRelativeTo(null);
-		window.setSize(new Dimension(1600,1000));
-			
-		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
-		panel.setBackground(Color.gray);
-		
-		JPasswordField pwField = new JPasswordField();
-		pwField.setPreferredSize(new Dimension(750,50));
 
-		JButton mainButton = new JButton("Main");
-		mainButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				window.setVisible(false);
-				initializeMain();
-			}
-		});
-		mainButton.setPreferredSize(new Dimension(750,150));
-		mainButton.setBackground(Color.green);
-		
-		panel.add(pwField);
-		panel.add(mainButton);
-
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		window.add(panel);
-		window.setVisible(true);
-	}
 	public void initializeMain()
 	{
 		window = new JFrame();
@@ -51,16 +20,10 @@ public class MainWindow implements ActionListener{
 		window.setSize(1600,1000);
 		window.setLocationRelativeTo(null);
 		
-		Font f = new Font("sans-serif", Font.PLAIN, 18);
-		UIManager.put("Menu.font", f);
-		UIManager.put("MenuItem.font", f);
-		UIManager.put("CheckBoxMenuItem.font", f);
-		UIManager.put("RadioButtonMenuItem.font", f);
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 		JMenuItem newMenuItem = new JMenuItem("New... ");
-
 		newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
 		newMenuItem.addActionListener(this);
 		
@@ -74,13 +37,7 @@ public class MainWindow implements ActionListener{
 		fileMenu.add(saveMenuItem);
 		fileMenu.add(exitMenuItem);
 		
-		fileMenu.add(new JRadioButtonMenuItem("RadioButton"));
-		fileMenu.add(new JCheckBoxMenuItem("CheckBox"));
-		fileMenu.add(new JMenu("Sub Menu"));
-		fileMenu.addSeparator();
-		
 		menuBar.add(fileMenu);
-		
 		window.setJMenuBar(menuBar);
 		
 		// Main Panel
@@ -95,11 +52,6 @@ public class MainWindow implements ActionListener{
 		button1.setPreferredSize(new Dimension(200,75));
 		button1.setFont(new Font("Arial", Font.PLAIN, 16));
 		button1.setMargin(new Insets(10,10,10,30));
-		button1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.print("Hello World!");
-			}
-		});
 		panel.add(button1);
 		
 		// Profile Button
@@ -109,11 +61,6 @@ public class MainWindow implements ActionListener{
 		button2.setMnemonic(KeyEvent.VK_P);
 		button2.setFont(new Font("Arial", Font.PLAIN, 16));
 		button2.setMargin(new Insets(10,10,10,30));
-		button2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.print("Hello World!");
-			}
-		});
 		panel.add(button2);
 		
 		// Planner Button
@@ -123,11 +70,6 @@ public class MainWindow implements ActionListener{
 		button3.setMnemonic(KeyEvent.VK_L);
 		button3.setFont(new Font("Arial", Font.PLAIN, 16));
 		button3.setMargin(new Insets(10,10,10,30));
-		button3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				initializePlanner();
-			}
-		});
 		button3.setBackground(Color.CYAN);
 		panel.add(button3);
 		
@@ -158,39 +100,29 @@ public class MainWindow implements ActionListener{
 		window.setSize(1600,1000);
 		window.setLocationRelativeTo(null);
 		
-		JPanel selector = new JPanel();
-		selector.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+		Font font = new Font("Arial", Font.BOLD, 16);
+		Insets inset = new Insets(10,10,10,30);
+		JPanel selector = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
+		Dimension dms = new Dimension(200, 75);
 		
 		JButton monthly = new JButton("Monthly");
-		monthly.setPreferredSize(new Dimension(200,75));
-		monthly.setFont(new Font("Arial", Font.PLAIN, 16));
-		monthly.setMargin(new Insets(10,10,10,30));
-		monthly.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				InitializeMonthly();
-			}
-		});
-		
+		monthly.setPreferredSize(dms);
+		monthly.setFont(font);
+		monthly.setMargin(inset);
+		monthly.addActionListener(e -> InitializeMonthly());
+
 		JButton weekly = new JButton("Weekly");
-		weekly.setPreferredSize(new Dimension(200,75));
-		weekly.setFont(new Font("Arial", Font.PLAIN, 16));
-		weekly.setMargin(new Insets(10,10,10,30));
-		weekly.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				initiateWeekly();
-			}
-		});
+		weekly.setPreferredSize(dms);
+		weekly.setFont(font);
+		weekly.setMargin(inset);
+		weekly.addActionListener(e -> initializeWeekly());
 		
 		JButton daily = new JButton("Daily");
-		daily.setPreferredSize(new Dimension(200,75));
-		daily.setFont(new Font("Arial", Font.PLAIN, 16));
-		daily.setMargin(new Insets(10,10,10,30));
-		daily.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				initializeDaily();
-			}
-		});
-		
+		daily.setPreferredSize(dms);
+		daily.setFont(font);
+		daily.setMargin(inset);
+		daily.addActionListener(e -> initializeDaily());
+
 		window.add(selector);
 		selector.add(weekly);
 		selector.add(daily);
@@ -204,11 +136,10 @@ public class MainWindow implements ActionListener{
 		JButton backButton = new JButton("Menu");
 		JPanel dayPanel = new JPanel();
 		JLabel current = new JLabel();
-		JMenuBar menuBar = new JMenuBar();
-		JMenu fileMenu = new JMenu("File");
-		JMenuItem newMenuItem = new JMenuItem("New... ");
-		JMenuItem saveMenuItem = new JMenuItem("Save");
-		JMenuItem exitMenuItem = new JMenuItem("Exit");
+		
+		Font font = new Font("Arial", Font.BOLD, 16);
+		Insets inset = new Insets(10,10,10,30);
+		Dimension dms = new Dimension(200, 75);
 		
 		window.setVisible(false);
 		window = new JFrame();
@@ -218,33 +149,16 @@ public class MainWindow implements ActionListener{
 		window.setLayout(new BorderLayout(30,30));
 		window.setLocationRelativeTo(null);
 		
-		current.setFont(new Font("Arial", Font.BOLD, 24));
-		fileMenu.setMnemonic(KeyEvent.VK_F);
-		newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-		newMenuItem.addActionListener(this);
-		saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
-		exitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
-		fileMenu.add(newMenuItem);
-		fileMenu.add(saveMenuItem);
-		fileMenu.add(exitMenuItem);
-		fileMenu.add(new JCheckBoxMenuItem("CheckBox"));
-		fileMenu.add(new JMenu("Sub Menu"));
-		fileMenu.addSeparator();
-		menuBar.add(fileMenu);
+		current.setFont(font);
 		dayPanel.setLayout(new BorderLayout(10, 10));
 		backButton.setToolTipText("Go back to the Menu");
 		backButton.setMnemonic(KeyEvent.VK_M);
-		backButton.setPreferredSize(new Dimension(200,75));
-		backButton.setFont(new Font("Arial", Font.PLAIN, 16));
-		backButton.setMargin(new Insets(10,10,10,30));
-		backButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				window.setVisible(false);
-				initializeMain();
-			}
-		});
+		backButton.setPreferredSize(dms);
+		backButton.setFont(font);
+		backButton.setMargin(inset);
+		backButton.addActionListener(e -> initializeMain()); 
+		
 		dayPanel.add(backButton, BorderLayout.WEST);
-		Font textFont = new Font("Arial", Font.BOLD, 24);
 		panel.add(new JLabel("Monday"));
 		panel.add(new JLabel("Tuesday"));
 		panel.add(new JLabel("Wednesday"));
@@ -255,19 +169,10 @@ public class MainWindow implements ActionListener{
 
 		for(int i = 8; i <= 42; i++)
 		{
-			JTextField textField = new JTextField("Text Box");
-			textField.setFont(textFont);
-			textField.setForeground(Color.blue);
-			textField.setBackground(Color.yellow);
+			JTextArea textField = new JTextArea("Text Box");
+			textField.setFont(font);
 			textField.setMargin(new Insets(2, 3, 2, 3));
-			
-			textField.addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent arg0) {
-					current.setText(textField.getText());
-				}
-				
-			});
+			textField.addActionListener(e -> current.setText(textField.getText()));
 			panel.add(textField);
 		}
 		
@@ -277,13 +182,13 @@ public class MainWindow implements ActionListener{
 		window.add(currentPanel, BorderLayout.SOUTH);
 		window.add(dayPanel, BorderLayout.NORTH);
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		window.setJMenuBar(menuBar);
 		window.pack();
 		window.setVisible(true);
 		
 	}
-	public void initiateWeekly()
+	public void initializeWeekly()
 	{
+		
 	}
 	public void initializeDaily()
 	{
