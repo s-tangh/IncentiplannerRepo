73d1dda5939ecc9e79aec4daf4d784b8f103950f
s-tangh <112664972+s-tangh@users.noreply.github.com>
2024-01-22 17:15:55 -0800

    Update MainWindow.java

--- MainWindow.java
+++ MainWindow.java
@@ -1,24 +1,19 @@
-package Main;
+package MainWindow;
 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class MainWindow implements ActionListener{
+public class MainWindow extends JFrame implements ActionListener{
 	private JPanel titlePanel;
-	private JFrame window ;
-	public MainWindow() 
-	{
-		initializeMain();
-	}
-
-	public void initializeMain()
-	{
-		window = new JFrame();
-		window.setTitle("Main");
-		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		window.setSize(1600,1000);
-		window.setLocationRelativeTo(null);
+	private int coins;
+	private ImageIcon coinImg= new ImageIcon("/Incentiplanner/src/MainWindow/coin.png");
+	public MainWindow(int coins) {
+		this.coins = coins;
+		this.setTitle("Main");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setPreferredSize(new Dimension(3200, 2000));
+		this.setLocationRelativeTo(null);
 		
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
@@ -38,12 +33,18 @@ public class MainWindow implements ActionListener{
 		fileMenu.add(exitMenuItem);
 		
 		menuBar.add(fileMenu);
-		window.setJMenuBar(menuBar);
+		this.setJMenuBar(menuBar);
 		
 		// Main Panel
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
+		
 		panel.setBackground(Color.gray);
+		
+		JPanel dayPanel = new JPanel();
+		JLabel coinage = new JLabel("" + coins);
+		coinage.setIcon(coinImg);
+		dayPanel.add(coinage, BorderLayout.EAST);
 		
 		// Shop Button
 		JButton button1 = new JButton("Shop");
@@ -52,6 +53,12 @@ public class MainWindow implements ActionListener{
 		button1.setPreferredSize(new Dimension(200,75));
 		button1.setFont(new Font("Arial", Font.PLAIN, 16));
 		button1.setMargin(new Insets(10,10,10,30));
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				close();
+				Shop shop = new Shop(coins);
+			}
+		});
 		panel.add(button1);
 		
 		// Profile Button
@@ -61,6 +68,12 @@ public class MainWindow implements ActionListener{
 		button2.setMnemonic(KeyEvent.VK_P);
 		button2.setFont(new Font("Arial", Font.PLAIN, 16));
 		button2.setMargin(new Insets(10,10,10,30));
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				close();
+				Profile profile = new Profile(coins);
+			}
+		});
 		panel.add(button2);
 		
 		// Planner Button
@@ -71,13 +84,19 @@ public class MainWindow implements ActionListener{
 		button3.setFont(new Font("Arial", Font.PLAIN, 16));
 		button3.setMargin(new Insets(10,10,10,30));
 		button3.setBackground(Color.CYAN);
+		button3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				close();
+				Planners planner = new Planners(coins);
+			}
+		});
 		panel.add(button3);
 		
 		// Jlabel is a display for a short text String
 		titlePanel = new JPanel();
 		titlePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 		titlePanel.setBackground(Color.DARK_GRAY);
-		window.add(titlePanel, BorderLayout.NORTH);
+		this.add(titlePanel, BorderLayout.NORTH);
 		
 		JLabel label = new JLabel("Incentiplanner - \"Incentivize your planning\"");
 		titlePanel.add(label);
@@ -88,117 +107,23 @@ public class MainWindow implements ActionListener{
 		label.setVerticalTextPosition(SwingConstants.BOTTOM);
 		
 		panel.setPreferredSize(new Dimension(250, 250));
-		window.add(panel, BorderLayout.WEST);
+		this.add(panel, BorderLayout.WEST);
+		this.add(dayPanel, BorderLayout.NORTH);
 		
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		window.setVisible(true);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		this.setVisible(true);
 	}
-	public void initializePlanner(){
-		window = new JFrame();
-		window.setTitle("Selection");
-		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		window.setSize(1600,1000);
-		window.setLocationRelativeTo(null);
-		
-		Font font = new Font("Arial", Font.BOLD, 16);
-		Insets inset = new Insets(10,10,10,30);
-		JPanel selector = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
-		Dimension dms = new Dimension(200, 75);
-		
-		JButton monthly = new JButton("Monthly");
-		monthly.setPreferredSize(dms);
-		monthly.setFont(font);
-		monthly.setMargin(inset);
-		monthly.addActionListener(e -> InitializeMonthly());
-
-		JButton weekly = new JButton("Weekly");
-		weekly.setPreferredSize(dms);
-		weekly.setFont(font);
-		weekly.setMargin(inset);
-		weekly.addActionListener(e -> initializeWeekly());
-		
-		JButton daily = new JButton("Daily");
-		daily.setPreferredSize(dms);
-		daily.setFont(font);
-		daily.setMargin(inset);
-		daily.addActionListener(e -> initializeDaily());
-
-		window.add(selector);
-		selector.add(weekly);
-		selector.add(daily);
-		selector.add(monthly);
-		selector.setVisible(true);
-		window.setVisible(true);
-	}
-	public void InitializeMonthly(){
-		JPanel panel = new JPanel(new GridLayout(6, 7, 5, 0));
-		JPanel currentPanel = new JPanel();
-		JButton backButton = new JButton("Menu");
-		JPanel dayPanel = new JPanel();
-		JLabel current = new JLabel();
-		
-		Font font = new Font("Arial", Font.BOLD, 16);
-		Insets inset = new Insets(10,10,10,30);
-		Dimension dms = new Dimension(200, 75);
-		
-		window.setVisible(false);
-		window = new JFrame();
-		window.setTitle("Monthly view");
-		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		window.setSize(1600,1000);
-		window.setLayout(new BorderLayout(30,30));
-		window.setLocationRelativeTo(null);
-		
-		current.setFont(font);
-		dayPanel.setLayout(new BorderLayout(10, 10));
-		backButton.setToolTipText("Go back to the Menu");
-		backButton.setMnemonic(KeyEvent.VK_M);
-		backButton.setPreferredSize(dms);
-		backButton.setFont(font);
-		backButton.setMargin(inset);
-		backButton.addActionListener(e -> initializeMain()); 
-		
-		dayPanel.add(backButton, BorderLayout.WEST);
-		panel.add(new JLabel("Monday"));
-		panel.add(new JLabel("Tuesday"));
-		panel.add(new JLabel("Wednesday"));
-		panel.add(new JLabel("Thursday"));
-		panel.add(new JLabel("Friday"));
-		panel.add(new JLabel("Saturday"));
-		panel.add(new JLabel("Sunday"));
-
-		for(int i = 8; i <= 42; i++)
-		{
-			JTextArea textField = new JTextArea("Text Box");
-			textField.setFont(font);
-			textField.setMargin(new Insets(2, 3, 2, 3));
-			textField.addActionListener(e -> current.setText(textField.getText()));
-			panel.add(textField);
-		}
-		
-		currentPanel.add(current, BorderLayout.CENTER);
-//		currentPanel.setPreferredSize(new Dimension(1000, 100));
-		window.add(panel, BorderLayout.CENTER);
-		window.add(currentPanel, BorderLayout.SOUTH);
-		window.add(dayPanel, BorderLayout.NORTH);
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		window.pack();
-		window.setVisible(true);
-		
-	}
-	public void initializeWeekly()
-	{
-		
-	}
-	public void initializeDaily()
-	{
-	}
+	
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		if(e.getSource() instanceof JMenuItem) {
 			JMenuItem item = (JMenuItem) e.getSource();
 			String text = item.getText();
 			System.out.println(text);
 		}
+	}
+	
+	public void close()
+	{
+		this.setVisible(false);
 	}
 }
